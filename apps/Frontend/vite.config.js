cd624bb2aa91c36e4e2561d85c7b44d3a7e4739b
Deependra Varshney <deependra03.jls@gmail.com>
2025-01-25 22:05:13 +0530

    some changes in the vite config

--- apps/Frontend/vite.config.js
+++ apps/Frontend/vite.config.js
@@ -1,17 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      external: []
+    }
+  },
   server: {
     port: 5178,
     proxy: {
       '/api': {
-        target: 'http://${process.env.VITE_REACT_APP_API_URL}',
+        target: process.env.VITE_REACT_APP_API_URL,
         changeOrigin: true,
-        secure: false,
-        ws: true
+        secure: false
       }
     }
   }
